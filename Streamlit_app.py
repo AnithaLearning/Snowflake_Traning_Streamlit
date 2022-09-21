3bdfb20ffc8d51d543c69af36c4113284f5a8df0
Anitha <114092745+AnithaLearning@users.noreply.github.com>
2022-09-21 18:10:28 +0100

    Update Streamlit_app.py
    Added few more text to display

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title ( 'The Home Care Service')
+
+streamlit.header ( 'Connect to the home care service person')
+
+streamlit.text ( 'Home care services can be found for the following Services')
+
+streamlit.text ( 'Cooking , Cleaning , Washing , Helping Elders')
