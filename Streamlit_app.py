b41ecc2920d2f64769f5ad4d2b57709c4c1336c6
Anitha <114092745+AnithaLearning@users.noreply.github.com>
2022-09-22 16:46:55 +0100

    Update Streamlit_app.py
    added pandas dataframe commands

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -10,3 +10,9 @@ streamlit.text ( ' 🐔 Home care services can be found for the following Servic
 streamlit.text ( ' 🥣 Cooking , Cleaning , Washing , Helping Elders')
 
 streamlit.header ( '🍌🥭 Find Your Own Home carer 🥝🍇' )
+
+import pandas
+
+my_fruit_list =pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
